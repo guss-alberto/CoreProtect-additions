64b1fcaa22373e0cdf05f267f585ee7a57be24eb
Alberto Gusmeroli <96133902+guss-alberto@users.noreply.github.com>
2026-02-19 14:43:13 +0100

    remove unused imports

--- src/main/java/com/alisaa/coreprotectadditions/Main.java
+++ src/main/java/com/alisaa/coreprotectadditions/Main.java
@@ -1,8 +1,5 @@
 package com.alisaa.coreprotectadditions;
 
-import net.coreprotect.CoreProtect;
-import net.coreprotect.CoreProtectAPI;
-
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
@@ -11,6 +8,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 import com.alisaa.coreprotectadditions.eventhandlers.*;
 
 public class Main extends JavaPlugin {
+    ApiWrapper api;
+
     @Override
     public void onEnable() {
         PluginManager pluginManager = Bukkit.getPluginManager();
@@ -20,10 +19,9 @@ public class Main extends JavaPlugin {
             pluginManager.disablePlugin(this);
             return;
         }
-        ApiWrapper api = new ApiWrapper();        
+        api = new ApiWrapper();        
 
         ConfigHandler.initConfig(this);
-
 
         if (ConfigHandler.LOG_LEASHES){
             pluginManager.registerEvents(new LeashLogger(api), this);
